6f672951d2b00c35d036596b235023d62f1d7b03
Christian <christian.toney@beastslash.com>
2026-01-17 12:10:41 -0500

    Remove unused code and improve formatting

--- src/routes/actions/{action_id}/mod.rs
+++ src/routes/actions/{action_id}/mod.rs
@@ -1,10 +1,38 @@
 use std::sync::Arc;
-
-use axum::{Extension, Json, Router, extract::{Path, State, rejection::JsonRejection}};
+use axum::{Extension, Json, Router, extract::{Path, State}};
 use reqwest::StatusCode;
 use uuid::Uuid;
-
-use crate::{AppState, HTTPError, middleware::authentication_middleware, resources::{access_policy::{AccessPolicy, AccessPolicyError, AccessPolicyPermissionLevel, AccessPolicyResourceType, EditableAccessPolicyProperties, ResourceHierarchy}, action::Action, http_transaction::HTTPTransaction, server_log_entry::ServerLogEntry, user::User}, utilities::{resource_hierarchy::{self, ResourceHierarchyError}, route_handler_utilities::{get_action_from_name, get_user_from_option_user, map_postgres_error_to_http_error, verify_user_permissions}}};
+use crate::{
+  AppState, 
+  HTTPError, 
+  middleware::authentication_middleware, 
+  resources::{
+    access_policy::{
+      AccessPolicyPermissionLevel, 
+      AccessPolicyResourceType, 
+      ResourceHierarchy
+    }, 
+    action::{
+      Action, 
+      ActionError
+    }, 
+    http_transaction::HTTPTransaction, 
+    server_log_entry::ServerLogEntry, 
+    user::User
+  }, 
+  utilities::{
+    resource_hierarchy::{
+      self, 
+      ResourceHierarchyError
+    }, 
+    route_handler_utilities::{
+      get_action_from_name, 
+      get_user_from_option_user, 
+      map_postgres_error_to_http_error, 
+      verify_user_permissions
+    }
+  }
+};
 
 async fn get_resource_hierarchy(action: &Action, http_transaction: &HTTPTransaction, mut postgres_client: &mut deadpool_postgres::Client) -> Result<ResourceHierarchy, HTTPError> {
 
@@ -44,56 +72,6 @@ async fn get_resource_hierarchy(action: &Action, http_transaction: &HTTPTransact
 
 }
 
-// async fn get_access_policy(access_policy_id: &str, http_transaction: &HTTPTransaction, mut postgres_client: &mut deadpool_postgres::Client) -> Result<AccessPolicy, HTTPError> {
-
-//   let access_policy_id = match Uuid::parse_str(&access_policy_id) {
-
-//     Ok(access_policy_id) => access_policy_id,
-
-//     Err(_) => {
-
-//       let http_error = HTTPError::BadRequestError(Some("You must provide a valid UUID for the access policy ID.".to_string()));
-//       let _ = ServerLogEntry::from_http_error(&http_error, Some(&http_transaction.id), &mut postgres_client).await;
-//       return Err(http_error);
-
-//     }
-
-//   };
-
-//   let _ = ServerLogEntry::trace(&format!("Getting access policy {}...", access_policy_id), Some(&http_transaction.id), &mut postgres_client).await;
-  
-//   let access_policy = match AccessPolicy::get_by_id(&access_policy_id, &mut postgres_client).await {
-
-//     Ok(access_policy) => access_policy,
-
-//     Err(error) => {
-
-//       let http_error = match error {
-//         AccessPolicyError::NotFoundError(_) => HTTPError::NotFoundError(Some(error.to_string())),
-//         AccessPolicyError::PostgresError(error) => {
-
-//           match error.as_db_error() {
-
-//             Some(error) => HTTPError::InternalServerError(Some(error.to_string())),
-//             None => HTTPError::InternalServerError(Some(error.to_string()))
-
-//           }
-
-//         }
-//         _ => HTTPError::InternalServerError(Some(error.to_string()))
-//       };
-//       let _ = ServerLogEntry::from_http_error(&http_error, Some(&http_transaction.id), &mut postgres_client).await;
-
-//       return Err(http_error);
-
-//     }
-
-//   };
-
-//   return Ok(access_policy);
-
-// }
-
 async fn get_action_from_id(action_id_string: &str, http_transaction: &HTTPTransaction, mut postgres_client: &mut deadpool_postgres::Client) -> Result<Action, HTTPError> {
 
   let action_id = match Uuid::parse_str(&action_id_string) {
@@ -110,14 +88,20 @@ async fn get_action_from_id(action_id_string: &str, http_transaction: &HTTPTrans
 
   };
 
-  let _ = ServerLogEntry::trace(&format!("Getting action {}", action_id), Some(&http_transaction.id), postgres_client).await;
+  let _ = ServerLogEntry::trace(&format!("Getting action {}...", action_id), Some(&http_transaction.id), postgres_client).await;
   let action = match Action::get_by_id(&action_id, postgres_client).await {
 
     Ok(action) => action,
 
     Err(error) => {
 
-      let http_error = HTTPError::InternalServerError(Some(format!("Failed to get action {}: {:?}", action_id, error)));
+      let http_error = match error {
+        
+        ActionError::NotFoundError(message) => HTTPError::NotFoundError(Some(message)),
+
+        error => HTTPError::InternalServerError(Some(format!("Failed to get action {}: {:?}", action_id, error)))
+
+      };
       let _ = http_error.print_and_save(Some(&http_transaction.id), &mut postgres_client).await;
       return Err(http_error);
 
