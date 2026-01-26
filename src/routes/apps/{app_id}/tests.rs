04bdaecca7771bf0e2820c7b59066a72a0750b0d
Christian <37517470+Christian-Toney@users.noreply.github.com>
2026-01-26 16:50:09 -0500

    Add test case to verify not found when getting app by ID

--- src/routes/apps/{app_id}/tests.rs
+++ src/routes/apps/{app_id}/tests.rs
@@ -177,38 +177,38 @@ async fn verify_permission_when_getting_app_by_id() -> Result<(), TestSlashstepS
 
 }
 
-// /// Verifies that the router can return a 404 status code if the requested action doesn't exist
-// #[tokio::test]
-// #[timeout(20000)]
-// async fn verify_not_found_when_getting_action_by_id() -> Result<(), TestSlashstepServerError> {
+/// Verifies that the router can return a 404 status code if the requested app doesn't exist
+#[tokio::test]
+#[timeout(20000)]
+async fn verify_not_found_when_getting_app_by_id() -> Result<(), TestSlashstepServerError> {
 
-//   let test_environment = TestEnvironment::new().await?;
-//   initialize_required_tables(&mut test_environment.postgres_pool.get().await?).await?;
+  let test_environment = TestEnvironment::new().await?;
+  initialize_required_tables(&mut test_environment.postgres_pool.get().await?).await?;
   
-//   // Create the user and the session.
-//   let user = test_environment.create_random_user().await?;
-//   let session = test_environment.create_session(&user.id).await?;
-//   let json_web_token_private_key = Session::get_json_web_token_private_key().await?;
-//   let session_token = session.generate_json_web_token(&json_web_token_private_key).await?;
+  // Create the user and the session.
+  let user = test_environment.create_random_user().await?;
+  let session = test_environment.create_session(&user.id).await?;
+  let json_web_token_private_key = Session::get_json_web_token_private_key().await?;
+  let session_token = session.generate_json_web_token(&json_web_token_private_key).await?;
 
-//   // Set up the server and send the request.
-//   let state = AppState {
-//     database_pool: test_environment.postgres_pool.clone(),
-//   };
-//   let router = super::get_router(state.clone())
-//     .layer(middleware::from_fn_with_state(state.clone(), http_request_middleware::create_http_request))
-//     .with_state(state)
-//     .into_make_service_with_connect_info::<SocketAddr>();
-//   let test_server = TestServer::new(router)?;
-//   let response = test_server.get(&format!("/actions/{}", uuid::Uuid::now_v7()))
-//     .add_cookie(Cookie::new("sessionToken", format!("Bearer {}", session_token)))
-//     .await;
+  // Set up the server and send the request.
+  let state = AppState {
+    database_pool: test_environment.postgres_pool.clone(),
+  };
+  let router = super::get_router(state.clone())
+    .layer(middleware::from_fn_with_state(state.clone(), http_request_middleware::create_http_request))
+    .with_state(state)
+    .into_make_service_with_connect_info::<SocketAddr>();
+  let test_server = TestServer::new(router)?;
+  let response = test_server.get(&format!("/apps/{}", uuid::Uuid::now_v7()))
+    .add_cookie(Cookie::new("sessionToken", format!("Bearer {}", session_token)))
+    .await;
   
-//   // Verify the response.
-//   assert_eq!(response.status_code(), 404);
-//   return Ok(());
+  // Verify the response.
+  assert_eq!(response.status_code(), 404);
+  return Ok(());
 
-// }
+}
 
 // /// Verifies that the router can return a 204 status code if the action is successfully deleted.
 // #[tokio::test]
