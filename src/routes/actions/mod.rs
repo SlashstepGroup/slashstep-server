bb0fe846d09d3ba9ab55bd0f82f731b40dc4b9ee
Christian <37517470+Christian-Toney@users.noreply.github.com>
2026-01-29 22:38:06 -0500

    Add app support for /actions

--- src/routes/actions/mod.rs
+++ src/routes/actions/mod.rs
@@ -1,21 +1,36 @@
+/**
+ * 
+ * Any functionality for /actions should be handled here.
+ * 
+ * Programmers: 
+ * - Christian Toney (https://christiantoney.com)
+ * 
+ * © 2026 Beastslash LLC
+ * 
+ */
+
 use std::sync::Arc;
 use axum::{Extension, Router, extract::{Query, State}};
 use axum_extra::response::ErasedJson;
-use crate::{AppState, HTTPError, middleware::{authentication_middleware, http_request_middleware}, resources::{access_policy::AccessPolicyResourceType, action_log_entry::ActionLogEntryTargetResourceType, http_transaction::HTTPTransaction, user::User}, utilities::reusable_route_handlers::{ActionListQueryParameters, list_actions}};
+use crate::{AppState, HTTPError, middleware::{authentication_middleware, http_request_middleware}, resources::{access_policy::AccessPolicyResourceType, action_log_entry::ActionLogEntryTargetResourceType, app::App, http_transaction::HTTPTransaction, user::User}, utilities::reusable_route_handlers::{ActionListQueryParameters, list_actions}};
 
 #[path = "./{action_id}/mod.rs"]
 pub mod action_id;
 
+/// GET /actions
+/// 
+/// Lists actions.
 #[axum::debug_handler]
 async fn handle_list_actions_request(
   Query(query_parameters): Query<ActionListQueryParameters>,
   State(state): State<AppState>, 
   Extension(http_transaction): Extension<Arc<HTTPTransaction>>,
-  Extension(user): Extension<Option<Arc<User>>>
+  Extension(user): Extension<Option<Arc<User>>>,
+  Extension(app): Extension<Option<Arc<App>>>
 ) -> Result<ErasedJson, HTTPError> {
 
   let resource_hierarchy = vec![(AccessPolicyResourceType::Instance, None)];
-  return list_actions(Query(query_parameters), State(state), Extension(http_transaction), Extension(user), resource_hierarchy, ActionLogEntryTargetResourceType::Instance, None).await;
+  return list_actions(Query(query_parameters), State(state), Extension(http_transaction), Extension(user), Extension(app), resource_hierarchy, ActionLogEntryTargetResourceType::Instance, None).await;
 
 }
 
@@ -24,6 +39,7 @@ pub fn get_router(state: AppState) -> Router<AppState> {
   let router = Router::<AppState>::new()
     .route("/actions", axum::routing::get(handle_list_actions_request))
     .layer(axum::middleware::from_fn_with_state(state.clone(), authentication_middleware::authenticate_user))
+    .layer(axum::middleware::from_fn_with_state(state.clone(), authentication_middleware::authenticate_app))
     .layer(axum::middleware::from_fn_with_state(state.clone(), http_request_middleware::create_http_request))
     .merge(action_id::get_router(state.clone()));
   return router;
