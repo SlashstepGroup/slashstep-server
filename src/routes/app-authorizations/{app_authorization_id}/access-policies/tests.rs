032fed11e59e25d3bb01ef979c63b791f342cd58
Christian <37517470+Christian-Toney@users.noreply.github.com>
2026-01-30 19:32:34 -0500

    Add test case to verify permission when listing access policies

--- src/routes/app-authorizations/{app_authorization_id}/access-policies/tests.rs
+++ src/routes/app-authorizations/{app_authorization_id}/access-policies/tests.rs
@@ -396,38 +396,38 @@ async fn verify_authentication_when_listing_access_policies() -> Result<(), Test
 
 }
 
-// /// Verifies that the server returns a 403 status code when the user lacks permissions and is authenticated.
-// #[tokio::test]
-// async fn verify_permission_when_listing_access_policies() -> Result<(), TestSlashstepServerError> {
+/// Verifies that the server returns a 403 status code when the user lacks permissions and is authenticated.
+#[tokio::test]
+async fn verify_permission_when_listing_access_policies() -> Result<(), TestSlashstepServerError> {
 
-//   let test_environment = TestEnvironment::new().await?;
-//   initialize_required_tables(&test_environment.database_pool).await?;
-//   initialize_predefined_actions(&test_environment.database_pool).await?;
-//   initialize_predefined_roles(&test_environment.database_pool).await?;
+  let test_environment = TestEnvironment::new().await?;
+  initialize_required_tables(&test_environment.database_pool).await?;
+  initialize_predefined_actions(&test_environment.database_pool).await?;
+  initialize_predefined_roles(&test_environment.database_pool).await?;
 
-//   // Create the user and the session.
-//   let user = test_environment.create_random_user().await?;
-//   let session = test_environment.create_session(&user.id).await?;
-//   let json_web_token_private_key = Session::get_json_web_token_private_key().await?;
-//   let session_token = session.generate_json_web_token(&json_web_token_private_key).await?;
+  // Create the user and the session.
+  let user = test_environment.create_random_user().await?;
+  let session = test_environment.create_session(&user.id).await?;
+  let json_web_token_private_key = Session::get_json_web_token_private_key().await?;
+  let session_token = session.generate_json_web_token(&json_web_token_private_key).await?;
 
-//   // Create a dummy action.
-//   let dummy_app_authorization = test_environment.create_random_app_authorization(&None).await?;
+  // Create a dummy action.
+  let dummy_app_authorization = test_environment.create_random_app_authorization(&None).await?;
 
-//   // Set up the server and send the request.
-//   let state = AppState {
-//     database_pool: test_environment.database_pool.clone(),
-//   };
-//   let router = super::get_router(state.clone())
-//     .with_state(state)
-//     .into_make_service_with_connect_info::<SocketAddr>();
-//   let test_server = TestServer::new(router)?;
-//   let response = test_server.get(&format!("/app-authorizations/{}/access-policies", &dummy_app_authorization.id))
-//     .add_cookie(Cookie::new("sessionToken", format!("Bearer {}", session_token)))
-//     .await;
+  // Set up the server and send the request.
+  let state = AppState {
+    database_pool: test_environment.database_pool.clone(),
+  };
+  let router = super::get_router(state.clone())
+    .with_state(state)
+    .into_make_service_with_connect_info::<SocketAddr>();
+  let test_server = TestServer::new(router)?;
+  let response = test_server.get(&format!("/app-authorizations/{}/access-policies", &dummy_app_authorization.id))
+    .add_cookie(Cookie::new("sessionToken", format!("Bearer {}", session_token)))
+    .await;
   
-//   assert_eq!(response.status_code(), StatusCode::FORBIDDEN);
+  assert_eq!(response.status_code(), StatusCode::FORBIDDEN);
 
-//   return Ok(());
+  return Ok(());
 
-// }
+}
