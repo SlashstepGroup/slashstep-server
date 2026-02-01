dbe65a531b9f5eccf821ea55671972cd0b0f25f2
Christian Toney <37517470+Christian-Toney@users.noreply.github.com>
2026-02-01 12:09:22 -0500

    Add feature: List app credentials (#261)

--- src/routes/app-credentials/mod.rs
+++ src/routes/app-credentials/mod.rs
@@ -11,52 +11,52 @@
 
 #[path = "./{app_credential_id}/mod.rs"]
 mod app_credential_id;
-// #[cfg(test)]
-// mod tests;
+#[cfg(test)]
+mod tests;
 
 use std::sync::Arc;
 use axum::{Extension, Router, extract::{Query, State}};
 use axum_extra::response::ErasedJson;
-use crate::{AppState, HTTPError, middleware::{authentication_middleware, http_request_middleware}, resources::{access_policy::AccessPolicyResourceType, action_log_entry::ActionLogEntryTargetResourceType, app::App, app_authorization::{AppAuthorization, DEFAULT_MAXIMUM_APP_AUTHORIZATION_LIST_LIMIT}, http_transaction::HTTPTransaction, user::User}, utilities::reusable_route_handlers::{ResourceListQueryParameters, list_resources}};
+use crate::{AppState, HTTPError, middleware::{authentication_middleware, http_request_middleware}, resources::{access_policy::AccessPolicyResourceType, action_log_entry::ActionLogEntryTargetResourceType, app::App, app_credential::{AppCredential, DEFAULT_MAXIMUM_APP_CREDENTIAL_LIST_LIMIT}, http_transaction::HTTPTransaction, user::User}, utilities::reusable_route_handlers::{ResourceListQueryParameters, list_resources}};
 
-// /// GET /app-credentials
-// /// 
-// /// Lists app authorizations.
-// #[axum::debug_handler]
-// async fn handle_list_app_authorizations_request(
-//   Query(query_parameters): Query<ResourceListQueryParameters>,
-//   State(state): State<AppState>, 
-//   Extension(http_transaction): Extension<Arc<HTTPTransaction>>,
-//   Extension(authenticated_user): Extension<Option<Arc<User>>>,
-//   Extension(authenticated_app): Extension<Option<Arc<App>>>
-// ) -> Result<ErasedJson, HTTPError> {
+/// GET /app-credentials
+/// 
+/// Lists app credentials.
+#[axum::debug_handler]
+async fn handle_list_app_credentials_request(
+  Query(query_parameters): Query<ResourceListQueryParameters>,
+  State(state): State<AppState>, 
+  Extension(http_transaction): Extension<Arc<HTTPTransaction>>,
+  Extension(authenticated_user): Extension<Option<Arc<User>>>,
+  Extension(authenticated_app): Extension<Option<Arc<App>>>
+) -> Result<ErasedJson, HTTPError> {
 
-//   let resource_hierarchy = vec![(AccessPolicyResourceType::Instance, None)];
-//   let response = list_resources(
-//     Query(query_parameters), 
-//     State(state), 
-//     Extension(http_transaction), 
-//     Extension(authenticated_user), 
-//     Extension(authenticated_app), 
-//     resource_hierarchy, 
-//     ActionLogEntryTargetResourceType::Instance, 
-//     None, 
-//     |query, database_pool, individual_principal| Box::new(AppAuthorization::count(query, database_pool, individual_principal)),
-//     |query, database_pool, individual_principal| Box::new(AppAuthorization::list(query, database_pool, individual_principal)),
-//     "slashstep.appAuthorizations.list", 
-//     DEFAULT_MAXIMUM_APP_AUTHORIZATION_LIST_LIMIT,
-//     "app authorizations",
-//     "app authorization"
-//   ).await;
+  let resource_hierarchy = vec![(AccessPolicyResourceType::Instance, None)];
+  let response = list_resources(
+    Query(query_parameters), 
+    State(state), 
+    Extension(http_transaction), 
+    Extension(authenticated_user), 
+    Extension(authenticated_app), 
+    resource_hierarchy, 
+    ActionLogEntryTargetResourceType::Instance, 
+    None, 
+    |query, database_pool, individual_principal| Box::new(AppCredential::count(query, database_pool, individual_principal)),
+    |query, database_pool, individual_principal| Box::new(AppCredential::list(query, database_pool, individual_principal)),
+    "slashstep.appCredentials.list", 
+    DEFAULT_MAXIMUM_APP_CREDENTIAL_LIST_LIMIT,
+    "app credentials",
+    "app credential"
+  ).await;
   
-//   return response;
+  return response;
 
-// }
+}
 
 pub fn get_router(state: AppState) -> Router<AppState> {
 
   let router = Router::<AppState>::new()
-    // .route("/app-credentials", axum::routing::get(handle_list_app_authorizations_request))
+    .route("/app-credentials", axum::routing::get(handle_list_app_credentials_request))
     .layer(axum::middleware::from_fn_with_state(state.clone(), authentication_middleware::authenticate_user))
     .layer(axum::middleware::from_fn_with_state(state.clone(), authentication_middleware::authenticate_app))
     .layer(axum::middleware::from_fn_with_state(state.clone(), http_request_middleware::create_http_request))
