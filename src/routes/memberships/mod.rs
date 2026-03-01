b2215d15c1bbfbf53cd8d42a7b70a70a4d5c0f09
Christian Toney <37517470+Christian-Toney@users.noreply.github.com>
2026-03-01 14:54:49 -0500

    Add feature: List memberships (#522)

--- src/routes/memberships/mod.rs
+++ src/routes/memberships/mod.rs
@@ -9,56 +9,56 @@
  * 
  */
 
-use std::sync::Arc;
-use axum::{Extension, Router, extract::{Query, State}};
-use axum_extra::response::ErasedJson;
-use crate::{AppState, HTTPError, middleware::{authentication_middleware, http_transaction_middleware}, resources::{access_policy::AccessPolicyResourceType, action_log_entry::ActionLogEntryTargetResourceType, app::{App, DEFAULT_MAXIMUM_APP_LIST_LIMIT}, app_authorization::AppAuthorization, http_transaction::HTTPTransaction, user::User}, utilities::reusable_route_handlers::{ResourceListQueryParameters, list_resources}};
-
 #[path = "./{membership_id}/mod.rs"]
 mod membership_id;
 #[cfg(test)]
 mod tests;
 
-// /// GET /memberships
-// /// 
-// /// Lists apps.
-// #[axum::debug_handler]
-// async fn handle_list_apps_request(
-//   Query(query_parameters): Query<ResourceListQueryParameters>,
-//   State(state): State<AppState>, 
-//   Extension(http_transaction): Extension<Arc<HTTPTransaction>>,
-//   Extension(authenticated_user): Extension<Option<Arc<User>>>,
-//   Extension(authenticated_app): Extension<Option<Arc<App>>>,
-//   Extension(authenticated_app_authorization): Extension<Option<Arc<AppAuthorization>>>
-// ) -> Result<ErasedJson, HTTPError> {
+use std::sync::Arc;
+use axum::{Extension, Router, extract::{Query, State}};
+use axum_extra::response::ErasedJson;
+use crate::{AppState, HTTPError, middleware::{authentication_middleware, http_transaction_middleware}, resources::{access_policy::AccessPolicyResourceType, action_log_entry::ActionLogEntryTargetResourceType, app::App, app_authorization::AppAuthorization, membership::{DEFAULT_MAXIMUM_RESOURCE_LIST_LIMIT, Membership}, http_transaction::HTTPTransaction, user::User}, utilities::reusable_route_handlers::{ResourceListQueryParameters, list_resources}};
 
-//   let resource_hierarchy = vec![(AccessPolicyResourceType::Server, None)];
-//   let response = list_resources(
-//     Query(query_parameters), 
-//     State(state), 
-//     Extension(http_transaction), 
-//     Extension(authenticated_user), 
-//     Extension(authenticated_app), 
-//     Extension(authenticated_app_authorization),
-//     resource_hierarchy, 
-//     ActionLogEntryTargetResourceType::Server, 
-//     None, 
-//     |query, database_pool, individual_principal| Box::new(App::count(query, database_pool, individual_principal)),
-//     |query, database_pool, individual_principal| Box::new(App::list(query, database_pool, individual_principal)),
-//     "apps.list", 
-//     DEFAULT_MAXIMUM_APP_LIST_LIMIT,
-//     "apps",
-//     "app"
-//   ).await;
+/// GET /memberships
+/// 
+/// Lists memberships.
+#[axum::debug_handler]
+async fn handle_list_memberships_request(
+  Query(query_parameters): Query<ResourceListQueryParameters>,
+  State(state): State<AppState>, 
+  Extension(http_transaction): Extension<Arc<HTTPTransaction>>,
+  Extension(authenticated_user): Extension<Option<Arc<User>>>,
+  Extension(authenticated_app): Extension<Option<Arc<App>>>,
+  Extension(authenticated_app_authorization): Extension<Option<Arc<AppAuthorization>>>
+) -> Result<ErasedJson, HTTPError> {
 
-//   return response;
+  let resource_hierarchy = vec![(AccessPolicyResourceType::Server, None)];
+  let response = list_resources(
+    Query(query_parameters), 
+    State(state), 
+    Extension(http_transaction), 
+    Extension(authenticated_user), 
+    Extension(authenticated_app), 
+    Extension(authenticated_app_authorization),
+    resource_hierarchy, 
+    ActionLogEntryTargetResourceType::Server,
+    None, 
+    |query, database_pool, individual_principal| Box::new(Membership::count(query, database_pool, individual_principal)),
+    |query, database_pool, individual_principal| Box::new(Membership::list(query, database_pool, individual_principal)),
+    "memberships.list", 
+    DEFAULT_MAXIMUM_RESOURCE_LIST_LIMIT,
+    "memberships",
+    "membership"
+  ).await;
 
-// }
+  return response;
+
+}
 
 pub fn get_router(state: AppState) -> Router<AppState> {
 
   let router = Router::<AppState>::new()
-    // .route("/memberships", axum::routing::get(handle_list_apps_request))
+    .route("/memberships", axum::routing::get(handle_list_memberships_request))
     .layer(axum::middleware::from_fn_with_state(state.clone(), authentication_middleware::authenticate_user))
     .layer(axum::middleware::from_fn_with_state(state.clone(), authentication_middleware::authenticate_app))
     .layer(axum::middleware::from_fn_with_state(state.clone(), http_transaction_middleware::create_http_transaction))
