5cbe1734acbbaca09e2f42ec35ff2c2d8fb32fef
Christian <37517470+Christian-Toney@users.noreply.github.com>
2026-06-04 00:08:55 +0000

    Create directory if it doesn't exist

--- scripts/generate-postgresql-password.sh
+++ scripts/generate-postgresql-password.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 script_directory="$(dirname "$0")"
 secrets_directory="$script_directory/../secrets"
+mkdir -p $secrets_directory
 tr -dc 'A-Za-z0-9' < /dev/urandom | head -c 16 > "$secrets_directory/postgresql-password.txt"
